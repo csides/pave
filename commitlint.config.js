99fb612423a61ae610389153878c1173a71bdf82
Sides <20118558+csides@users.noreply.github.com>
2026-06-14 11:53:35 -0700

    build(repo): add repo scaffolding

---
+++ commitlint.config.js
@@ -0,0 +1,5 @@
+// Conventional Commits rules for commitlint (run via the commit-msg hook).
+// https://www.conventionalcommits.org
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+};
